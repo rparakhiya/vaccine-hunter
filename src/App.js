0c2a2647a7577b68f4556055bde06c6a43935b7c
Rumit Parakhiya <rumit.parakhiya@tarktech.com>
2021-05-30 10:14:21 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,145 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import { fetchVaccineAvailability } from './api';
+import { Checkbox, Table } from 'semantic-ui-react'
+import { useEffect } from 'react';
+import dayjs from 'dayjs';
+import _ from 'lodash';
 
 function App() {
+  const [vaccineAvailability, setVaccineAvailability] = useState([]);
+  const [dataUpdatedOn, setDataUpdatedOn] = useState(null);
+  const [searchArea, setSearchArea] = useState('pincode');
+  const [showAvailableCentersOnly, setShowAvailableCentersOnly] = useState(true);
+
+  function refreshData() {
+    return fetchVaccineAvailability(searchArea)
+        .then(availability => {
+          setVaccineAvailability(availability);
+          setDataUpdatedOn(dayjs());
+        });
+  }
+
+  useEffect(() => {
+    refreshData(searchArea);
+  }, [searchArea]);
+
+  useEffect(() => {
+    const timer = setInterval(refreshData,
+      60000
+    );
+    return () => clearInterval(timer);
+  }, [vaccineAvailability]);
+
+  function getRowClassName(availableDoses) {
+    return availableDoses > 10 ? 'available' :
+      availableDoses > 0 ? 'few_available' :
+      'unavailable';
+  }
+
+  function renderCenter(availability) {
+    return _(availability?.centers)?.map(center => {
+        const sessions = center.sessions.filter(session => session.min_age_limit === 18);
+        const doses = sessions.reduce((totalDoses, session) => totalDoses + session.available_capacity_dose1, 0);
+        
+        return {
+          date: availability.date,
+          name: center.name,
+          center_id: center.center_id,
+          address: center.address,
+          doses: doses
+        };
+      })
+      .sortBy(row => row.doses)
+      .reverse().value()
+      .filter(row => row.doses || !showAvailableCentersOnly)
+      .map(row => (<Table.Row key={`${row.date}_${row.center_id}`} className={getRowClassName(row.doses)}>
+            <Table.Cell>{row.date}</Table.Cell>
+            <Table.Cell>{row.name}</Table.Cell>
+            <Table.Cell>{row.address}</Table.Cell>
+            <Table.Cell>{row.doses}</Table.Cell>
+        </Table.Row>)
+      );
+  }
+
+  function renderRows(vaccineAvailability) {
+    const rows = _(vaccineAvailability)?.flatMap(availability => renderCenter(availability)).value();
+    return rows.length > 0 ? rows : (
+      <Table.Row>
+        <Table.Cell colSpan="4" textAlign="center">
+          No slots available! 😕
+        </Table.Cell>
+      </Table.Row>
+    );
+  }
+
+  function searchAreaChanged(e, {value}) {
+    setSearchArea(value);
+  }
+
+  function RenderFilterControls() {
+    return (
+      <>
+        <Checkbox
+          radio
+          label='Pincode'
+          name='location'
+          value='pincode'
+          checked={searchArea === 'pincode'}
+          onChange={searchAreaChanged}
+          className="filter-controls"
+        />
+        <Checkbox
+          radio
+          label='District'
+          name='location'
+          value='district'
+          checked={searchArea === 'district'}
+          onChange={searchAreaChanged}
+          className="filter-controls"
+        />
+        <Checkbox
+          toggle
+          label='Show Available Centers Only'
+          name='available_centers_only'
+          value='showAvailableCentersOnly'
+          checked={showAvailableCentersOnly}
+          onChange={(e, {checked}) => setShowAvailableCentersOnly(checked)}
+          className="filter-controls"
+          style={{float: 'right'}}
+        />
+      </>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell colSpan="4">
+                {RenderFilterControls()}
+            </Table.HeaderCell>
+          </Table.Row>
+          <Table.Row>
+            <Table.HeaderCell>Date</Table.HeaderCell>
+            <Table.HeaderCell>Center</Table.HeaderCell>
+            <Table.HeaderCell>Address</Table.HeaderCell>
+            <Table.HeaderCell>Available Slots (18+)</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {renderRows(vaccineAvailability)}
+        </Table.Body>
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell colSpan='4'>
+              Last updated on: {dataUpdatedOn?.format('DD-MM-YYYY hh:mm:ss a')}
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
     </div>
   );
 }
