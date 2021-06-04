98f98f2c99e6c357d78eeb3a3bad4dbf1b35b60b
Rumit Parakhiya <rumit.parakhiya@tarktech.com>
2021-06-04 16:44:01 +0530

    Show information in header

--- pages/[searchArea]/[areaCode]/dose/[doseNumber]/[ageLimit].js
+++ pages/[searchArea]/[areaCode]/dose/[doseNumber]/[ageLimit].js
@@ -3,8 +3,9 @@ import styles from "../../../../../styles/Home.module.css";
 import AvailabilityTracker from "../../../../../components/AvailabilityTracker";
 import { useRouter } from "next/router";
 import { fetchVaccineAvailability } from "../../../../../components/fetchVaccineAvailability";
+import { Segment, List } from "semantic-ui-react";
 
-export default function AvailabilityByPincode({vaccineAvailability}) {
+export default function AvailabilityByPincode({ vaccineAvailability }) {
   const router = useRouter();
   const { searchArea, areaCode, ageLimit, doseNumber } = router.query;
 
@@ -14,7 +15,19 @@ export default function AvailabilityByPincode({vaccineAvailability}) {
         <title>Check Vaccine availability</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Segment className={styles.header}>
+        <List>
+          <List.Item>
+            <strong>{searchArea}</strong>: {areaCode}
+          </List.Item>
+          <List.Item>
+            <strong>Age:</strong> {ageLimit}+
+          </List.Item>
+          <List.Item>
+            <strong>Dose:</strong> {doseNumber}
+          </List.Item>
+        </List>
+      </Segment>
       <main className={styles.main}>
         <AvailabilityTracker
           ageLimit="18"
@@ -32,7 +45,10 @@ export default function AvailabilityByPincode({vaccineAvailability}) {
 }
 
 export async function getServerSideProps(context) {
-  const {searchArea, areaCode} = context.params;
-  const vaccineAvailability = await fetchVaccineAvailability(searchArea, areaCode);
+  const { searchArea, areaCode } = context.params;
+  const vaccineAvailability = await fetchVaccineAvailability(
+    searchArea,
+    areaCode
+  );
   return { props: { vaccineAvailability } };
 }
